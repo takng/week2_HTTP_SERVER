9335fd5304f4fa25b862fbc5b4d1988bbb961944
TakNg <tak_ng@yahoo.com>
2017-07-06 15:28:05 +0000

    http

--- express_server.js
+++ express_server.js
@@ -17,6 +17,12 @@ function generateRandomString() {
 
 }
 
+app.post('/login', (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(req.body.username);
+  res.redirect("/urls");
+});
+
 app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -30,6 +36,13 @@ app.get('/u/:shortURL', (req, res) => {
 //  let longURL = urlDatabase[shortURL];
 //  res.redirect(longURL);
 //});
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  console.log(req.body.newURL);
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  //res.render("urls");
+});
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
@@ -58,17 +71,32 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.shortURL]
-  };
+  let templateVars =
+    //{ urls: {
+    {
+              shortURL: req.params.id,
+              longURL: urlDatabase[req.params.id]
+    };
+//            }
+    //};
   //let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+// load main page
+app.get('/', (req, res) => {
+  // this registers a handler on the ROOT path '/'
+  // if (req.session.user_id) {
+  //   res.redirect('/urls');
+  // } else {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index');
+//  };
 });
+
+// app.get("/", (req, res) => {
+//   res.end("Hello!");
+// });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
